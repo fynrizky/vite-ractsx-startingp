480786d0f314532e7de1284eb16b670ddbd4f865
fynrizky <fiyaniky25@gmail.com>
2023-02-19 15:42:59 +0700

    menambah tailwind dan responsive mobile and dekstop version

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const About = () => {
   return (
-    <div id='about' className='menu'>
-        <div className="row-menu">
+    <div id='about' className='menu bg-neutral-800'>
+        <div className="row-men text-zinc-300">
         <a href="#home">About</a>
         </div>
     </div>
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,13 +2,17 @@ import React, { useState } from 'react'
 
 
 export const Home: React.FC = () => {
-const [title, setTitle] = useState("Home");
+const [title, setTitle] = useState("Rizky Fiyannur");
 
   return (
     <>
-    <div id='home' className='menu'>
-      <div className='row-menu'>
+    <div id='home' className='menu bg-neutral-900 '>
+      <div className='row-menu mt-4 max-sm:mt-24 text-zinc-300 w-9/12 '>
+        <div className='text-center'>
           <a href="#about"> { (setTitle) ? title : ''} </a>
+        </div>
+        <img src="https://i.pinimg.com/474x/f0/39/ab/f039ab3707e47598ce379924528374df.jpg" 
+        className="border-4 mt-8 max-sm:mt-12 border-zinc-500 w-60 mx-auto p-1 rounded-full" alt="Avatar" />
       </div>
     </div>
     </>
